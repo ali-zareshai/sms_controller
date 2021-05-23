7bcb7215e3e6c5c2cba67865f320494ca34b6c61
ali_zareshaho <alititi96@gmail.com>
2021-05-23 22:14:23 +0430

    fix send sms

--- app/src/main/java/com/bita/smscontrol/activity/MainActivity.kt
+++ app/src/main/java/com/bita/smscontrol/activity/MainActivity.kt
@@ -2,18 +2,21 @@ package com.bita.smscontrol.activity
 
 import android.Manifest
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.telephony.SmsManager
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import cn.pedant.SweetAlert.SweetAlertDialog
 import com.bita.smscontrol.R
 import com.suke.widget.SwitchButton
 import com.valdesekamdem.library.mdtoast.MDToast
+
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
     var  smsManager:SmsManager?=null
@@ -63,21 +66,36 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         reportBtn?.setOnClickListener(this)
     }
 
-    private fun requestSmsPermission(permission:String) {
+    private fun requestSmsPermission(permission: String) {
         val grant = ContextCompat.checkSelfPermission(this, permission)
         if (grant != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(permission), REQUEST_CODE_SMS_PERMISSION)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(permission),
+                REQUEST_CODE_SMS_PERMISSION
+            )
         }
     }
 
-    private fun sendSMS(phoneNumber:String,message:String){
-        smsManager?.sendTextMessage(phoneNumber, null, message, null, null);
+    private fun sendSMS(phoneNumber: String, message: String){
+        val pDialog = SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE)
+        pDialog.progressHelper.barColor = Color.parseColor("#A5DC86")
+        pDialog.titleText = getString(R.string.sending)
+        pDialog.setCancelable(false)
+        pDialog.show()
+        smsManager?.sendTextMessage(phoneNumber, null, message, null, null)
+        Handler().postDelayed(object:Runnable{
+            override fun run() {
+                pDialog.dismissWithAnimation()
+            }
+        },10000);
+
     }
 
     override fun onClick(p0: View?) {
         when(p0){
-            sendBtn->sendParameters()
-            reportBtn->getReport()
+            sendBtn -> sendParameters()
+            reportBtn -> getReport()
         }
     }
 
@@ -87,23 +105,48 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     private fun sendParameters() {
         if(!isFilledAllFields()){
-            MDToast.makeText(this,getString(R.string.fill_all_fields),MDToast.LENGTH_LONG,MDToast.TYPE_ERROR).show()
+            MDToast.makeText(
+                this,
+                getString(R.string.fill_all_fields),
+                MDToast.LENGTH_LONG,
+                MDToast.TYPE_ERROR
+            ).show()
             return
         }
         if(isValidAllFields()){
             val message:String ="*setall*${onTimeEdit?.text.toString()}*${offTimeEdit?.text.toString()}*${delayConEdit?.text.toString()}" +
-                    "*${delayDisEdit?.text.toString()}*${maxAmpherEdit?.text.toString()}*${minAmpherEdit?.text.toString()}*${phoneNumberEdit?.text.toString()}"
+                    "*${delayDisEdit?.text.toString()}*${maxAmpherEdit?.text.toString()}*${minAmpherEdit?.text.toString()}*${phoneNumberEdit?.text.toString()}*"
 
-            Log.e("message::=>",message)
-            Log.e("phone::",devicePhoneNumberEdit?.text.toString())
+            showWaringDialog(devicePhoneNumberEdit?.text.toString(), message)
         }
 
+    }
+
+    private fun showWaringDialog(phoneNumber: String, message: String) {
+        SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+            .setTitleText(getString(R.string.are_you_sure))
+            .setContentText(getString(R.string.are_you_send_config))
+            .setConfirmText(getString(R.string.yes_send))
+            .setCancelText(getString(R.string.no))
+            .setConfirmClickListener(object : SweetAlertDialog.OnSweetClickListener {
+                override fun onClick(sweetAlertDialog: SweetAlertDialog?) {
+                    sendSMS(phoneNumber, message)
+                    sweetAlertDialog?.dismissWithAnimation()
+                }
+
+            })
+            .show()
     }
 
     private fun isValidAllFields(): Boolean {
         val mobileRegex =Regex("09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}")
         if(!mobileRegex.containsMatchIn(devicePhoneNumberEdit?.text.toString())){
-            MDToast.makeText(this,getString(R.string.not_valid_phone_number),MDToast.LENGTH_LONG,MDToast.TYPE_ERROR).show()
+            MDToast.makeText(
+                this,
+                getString(R.string.not_valid_phone_number),
+                MDToast.LENGTH_LONG,
+                MDToast.TYPE_ERROR
+            ).show()
             devicePhoneNumberEdit?.setBackgroundResource(R.drawable.border_edittext_error)
             return false;
         }else{
@@ -112,7 +155,12 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
         if(delayConEdit?.text.toString().toInt()>240){
             delayConEdit?.setBackgroundResource(R.drawable.border_edittext_error)
-            MDToast.makeText(this,getString(R.string.low_from_240),MDToast.LENGTH_LONG,MDToast.TYPE_ERROR).show()
+            MDToast.makeText(
+                this,
+                getString(R.string.low_from_240),
+                MDToast.LENGTH_LONG,
+                MDToast.TYPE_ERROR
+            ).show()
             return false
         }else{
             delayConEdit?.setBackgroundResource(R.drawable.border_edittext_normal)
@@ -120,14 +168,24 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
         if(delayDisEdit?.text.toString().toInt()>15){
             delayDisEdit?.setBackgroundResource(R.drawable.border_edittext_error)
-            MDToast.makeText(this,getString(R.string.low_from_240),MDToast.LENGTH_LONG,MDToast.TYPE_ERROR).show()
+            MDToast.makeText(
+                this,
+                getString(R.string.low_from_240),
+                MDToast.LENGTH_LONG,
+                MDToast.TYPE_ERROR
+            ).show()
             return false
         }else{
             delayDisEdit?.setBackgroundResource(R.drawable.border_edittext_normal)
         }
 
         if(!mobileRegex.containsMatchIn(phoneNumberEdit?.text.toString())){
-            MDToast.makeText(this,getString(R.string.not_valid_phone_number),MDToast.LENGTH_LONG,MDToast.TYPE_ERROR).show()
+            MDToast.makeText(
+                this,
+                getString(R.string.not_valid_phone_number),
+                MDToast.LENGTH_LONG,
+                MDToast.TYPE_ERROR
+            ).show()
             phoneNumberEdit?.setBackgroundResource(R.drawable.border_edittext_error)
             return false;
         }else{
