c0af4de5222511d10abe01f1c65566bf950a9591
ali_zareshaho <alititi96@gmail.com>
2021-05-26 11:18:58 +0430

    add feild ui

--- app/src/main/java/com/bita/smscontrol/receiver/SMSReceiver.kt
+++ app/src/main/java/com/bita/smscontrol/receiver/SMSReceiver.kt
@@ -1,12 +1,17 @@
 package com.bita.smscontrol.receiver
 
+import android.app.ActivityManager
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.ACTIVITY_SERVICE
 import android.content.Intent
+import android.os.Handler
 import android.provider.Telephony
-import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
+import com.bita.smscontrol.activity.MainActivity
 import com.bita.smscontrol.event.NewSms
 import org.greenrobot.eventbus.EventBus
+
 
 class SMSReceiver : BroadcastReceiver() {
     companion object {
@@ -18,9 +23,27 @@ class SMSReceiver : BroadcastReceiver() {
         val extractMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
         for (SmsMessage in extractMessages){
             val message =SmsMessage.displayMessageBody.trim()
-            if (message.startsWith("on_time",true)){
-                EventBus.getDefault().post(NewSms(SmsMessage.displayOriginatingAddress,message))
+            if (message.startsWith("on_time", true)){
+                if(isForeground(context,"com.bita.smscontrol")){
+                    EventBus.getDefault().post(NewSms(SmsMessage.displayOriginatingAddress, message))
+                }else{
+                    val intent = Intent(context,MainActivity::class.java)
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context?.startActivity(intent)
+                    Handler().postDelayed(object:Runnable{
+                        override fun run() {
+                            EventBus.getDefault().post(NewSms(SmsMessage.displayOriginatingAddress, message))
+                        }
+                    },4000);
+                }
             }
         }
+    }
+
+    fun isForeground(context:Context? ,myPackage: String): Boolean {
+        val manager = context?.getSystemService(ACTIVITY_SERVICE) as ActivityManager?
+        val runningTaskInfo = manager!!.getRunningTasks(1)
+        val componentInfo = runningTaskInfo[0].topActivity
+        return componentInfo!!.packageName == myPackage
     }
 }
