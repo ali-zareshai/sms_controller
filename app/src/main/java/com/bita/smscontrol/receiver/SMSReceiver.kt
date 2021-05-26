b9104a5502763ca7395c17a1988e6f6d3eb76aa1
ali_zareshaho <alititi96@gmail.com>
2021-05-26 19:26:48 +0430

    fix commit

--- app/src/main/java/com/bita/smscontrol/receiver/SMSReceiver.kt
+++ app/src/main/java/com/bita/smscontrol/receiver/SMSReceiver.kt
@@ -8,7 +8,6 @@ import android.content.Intent
 import android.os.Handler
 import android.provider.Telephony
 import android.util.Log
-import androidx.core.content.ContextCompat.getSystemService
 import com.bita.smscontrol.activity.MainActivity
 import com.bita.smscontrol.event.NewSms
 import org.greenrobot.eventbus.EventBus
@@ -25,24 +24,36 @@ class SMSReceiver : BroadcastReceiver() {
         for (SmsMessage in extractMessages){
             val message =SmsMessage.displayMessageBody.trim()
             if (message.startsWith("on_time", true)){
-                if(isForeground(context,"com.bita.smscontrol")){
-                    EventBus.getDefault().post(NewSms(SmsMessage.displayOriginatingAddress, message))
+                if(isForeground(context, "com.bita.smscontrol")){
+                    EventBus.getDefault().post(
+                        NewSms(
+                            SmsMessage.displayOriginatingAddress,
+                            message
+                        )
+                    )
                 }else{
-                    Log.e("SMSReceiver","start main activity ........")
-                    val intent = Intent(context,MainActivity::class.java)
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    context?.startActivity(intent)
-                    Handler().postDelayed(object:Runnable{
+                    Log.e("SMSReceiver", "start main activity ........")
+                    val i = Intent()
+                    i.setClass(context!!,MainActivity::class.java)
+                    i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    context!!.startActivity(i)
+
+                    Handler().postDelayed(object : Runnable {
                         override fun run() {
-                            EventBus.getDefault().post(NewSms(SmsMessage.displayOriginatingAddress, message))
+                            EventBus.getDefault().post(
+                                NewSms(
+                                    SmsMessage.displayOriginatingAddress,
+                                    message
+                                )
+                            )
                         }
-                    },4000)
+                    }, 4000)
                 }
             }
         }
     }
 
-    fun isForeground(context:Context? ,myPackage: String): Boolean {
+    fun isForeground(context: Context?, myPackage: String): Boolean {
         val manager = context?.getSystemService(ACTIVITY_SERVICE) as ActivityManager?
         val runningTaskInfo = manager!!.getRunningTasks(1)
         val componentInfo = runningTaskInfo[0].topActivity
